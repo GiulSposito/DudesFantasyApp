fefac219a5d3e50ee5f038ff1e016dda5f429588
Giuliano Sposito <gsposito@MMB026334CPS.local>
2023-10-12 11:46:43 -0300

    draft scripts to generate the weekly nfl fantay trophies

---
+++ R/snippets/weekly_trophies.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(dm)
+
+stats <- readRDS("./data/nfl_stats_db.rds")
+playersDB <- readRDS("./data/nfl_players_db.rds")
+round <- readRDS("./data/nfl_round_db.rds")
+teams <- readRDS("./data/nfl_teams_db.rds")
+
+
+round |> 
+  dm_draw(view_type = "all", column_types = T)
+
+
+rosters <- round$nfl_teams_rosters |> 
+  filter(tag=="final", week==5)
+
+points <- stats$nfl_players_points |> 
+  filter(season==2023, week==5)
+
+players <- playersDB$nfl_players |> 
+  select(playerId, name, position)
+
+rosters |> 
+  inner_join(points, by = join_by(season, week, playerId)) |> 
+  inner_join(playersDB$nfl_players, by = join_by(playerId)) |> 
+  select(teamId, slotPosition, rosterSlotId, playerId, player=name, position, pts) |> 
+  inner_join(teams$nfl_teams, by = join_by(teamId)) |> 
+  select(player, position, pts, team=name) |> 
+  slice_max(pts, n=1)
+  
+
+rosters |> 
+  inner_join(points, by = join_by(season, week, playerId)) |> 
+  inner_join(playersDB$nfl_players, by = join_by(playerId)) |> 
+  select(teamId, slotPosition, rosterSlotId, playerId, player=name, position, pts) |> 
+  inner_join(teams$nfl_teams, by = join_by(teamId)) |> 
+  filter(rosterSlotId >= 20) |> 
+  select(player, position, pts, team=name) |> 
+  slice_max(pts, n=1)
+
+rosters |> 
+  inner_join(points, by = join_by(season, week, playerId)) |> 
+  inner_join(playersDB$nfl_players, by = join_by(playerId)) |> 
+  select(teamId, slotPosition, rosterSlotId, playerId, player=name, position, pts) |> 
+  inner_join(teams$nfl_teams, by = join_by(teamId)) |> 
+  filter(rosterSlotId < 20) |> 
+  select(player, position, pts, team=name) |> 
+  group_by(team) |> 
+  slice_max(pts, n=2) |> 
+  mutate(pts_total=sum(pts)) |> 
+  ungroup() |> 
+  slice_max(pts_total, n=1)
+
+
+
+
