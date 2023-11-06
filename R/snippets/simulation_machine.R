d74716a6b9c4acb702a2afe60223053463e9d80d
Giuliano Sposito <gsposito@MMB026334CPS.local>
2023-11-06 19:22:28 -0300

    Adiciona "position" na projeção histórica Cria seeds com resultado da season Cria seeds combinando a current season + proj_src + proj_errors

--- R/snippets/simulation_machine.R
+++ R/snippets/simulation_machine.R
@@ -10,6 +10,7 @@ SEASON <- 2023
 # DATABASES
 ffa <- readRDS("./data/ffa_db.rds") # projecoes
 stt <- readRDS("./data/nfl_stats_db.rds") # pontuacao
+ply <- readRDS("./data/nfl_players_db.rds") # players info
 
 # id-map
 id_map <- ffa$ffa_player_ids |>
@@ -32,7 +33,10 @@ proj_source <- ffa$ffa_proj_source_points |>
 # point hist
 points <- stt$nfl_players_points |>
   filter(week != 0) |>
-  inner_join(id_map, join_by(playerId))
+  inner_join(id_map, join_by(playerId)) |>
+  inner_join(select(ply$nfl_players, playerId, pos = position),
+             by = join_by(playerId)) |>
+  mutate(pos = if_else(pos == "DEF", "DST", pos))
 
 # proj errors
 source("./R/transformation/simulation.R")
@@ -44,14 +48,14 @@ proj_w_errors <- applyErrorToProjection(proj_source, errors)
 dudes_simSeeds <- proj_table |>
   mutate(simType = paste0("proj_table_", avg_type),
          seeds = map(points, c)) |>
-  select(-avg_type,-points)
+  select(-avg_type, -points)
 
 # O VALOR DO PROJ_SOURCE DA NFL - 1 PTS
 dudes_simSeeds <- proj_source |>
   filter(data_src == "NFL") |>
   mutate(simType = data_src,
          seeds = map(points, c)) |>
-  select(-data_src,-points,-tag,-timestamp) |>
+  select(-data_src, -points, -tag, -timestamp) |>
   bind_rows(dudes_simSeeds)
 
 # MONTECARLO (PROJ SOURCE) => N
@@ -141,7 +145,7 @@ dudes_simSeeds <- 1:WEEK |>
       filter(season < .SEASON |
                (season == .SEASON & week < .WEEK)) |>
       filter(week != 0) |>
-      nest(pts = pts, .by = c(id, playerId)) |>
+      nest(pts = pts, .by = c(id, playerId, pos)) |>
       mutate(simType = "hist_data",
              seeds = map(pts, ~ .x$pts)) |>
       select(-pts) |>
@@ -153,9 +157,10 @@ dudes_simSeeds <- 1:WEEK |>
 dudes_simSeeds <- 1:WEEK |>
   map_df(\(.WEEK, .SEASON, .points) {
     .points |>
-      filter(season < .SEASON | (season == .SEASON & week < .WEEK)) |>
+      filter(season < .SEASON |
+               (season == .SEASON & week < .WEEK)) |>
       filter(week != 0) |>
-      nest(pts = pts, .by = c(id, playerId)) |>
+      nest(pts = pts, .by = c(id, playerId, pos)) |>
       mutate(simType = "hist_data_density",
              seeds = map(pts, \(pts) {
                if (nrow(pts) < 2)
@@ -205,9 +210,8 @@ dudes_simSeeds <-
              return(c(ptsA, ptsB, sample(ptsA, lenB - lenA, replace = T)))
            
          })) |>
-  select(-projPtsErrors, -points) |>
+  select(-projPtsErrors,-points) |>
   bind_rows(dudes_simSeeds)
-
 
 # DENSITY (PROJ SOURCE*BALANCED + ERRORS) => N
 dudes_simSeeds <-
@@ -232,36 +236,114 @@ dudes_simSeeds <-
   mutate(simType = "proj_src_w_errors_balanced_density",
          seeds = map2(projPtsErrors, points, \(ptsA, ptsB) {
            # 100 sampling from ptsA set
-           if(length(ptsA)<2) {
-             respA <- rep(ptsA, 100)             
+           if (length(ptsA) < 2) {
+             respA <- rep(ptsA, 100)
            } else {
              denA <- density(ptsA)
-             iA <- sample(length(denA$x), 100, replace = T, prob = denA$y)
+             iA <-
+               sample(length(denA$x),
+                      100,
+                      replace = T,
+                      prob = denA$y)
              respA <- denA$x[iA]
            }
            
            # 100 sampling from ptsB set
-           if(length(ptsB)<2) {
-             respB <- rep(ptsB, 100)             
+           if (length(ptsB) < 2) {
+             respB <- rep(ptsB, 100)
            } else {
              denB <- density(ptsB)
-             iB <- sample(length(denB$x), 100, replace = T, prob = denB$y)
+             iB <-
+               sample(length(denB$x),
+                      100,
+                      replace = T,
+                      prob = denB$y)
              respB <- denB$x[iB]
            }
            
            denAB <- density(c(respA, respB))
-           iAB <- sample(length(denAB$x), 100, replace = T, prob = denAB$y)
-
+           iAB <-
+             sample(length(denAB$x), 100, replace = T, prob = denAB$y)
+           
            # retorna os dois samplings
            return(denAB$x[iAB])
-
+           
          })) |>
-  select(-projPtsErrors, -points) |>
-  bind_rows(dudes_simSeeds) |> 
+  select(-projPtsErrors,-points) |>
+  bind_rows(dudes_simSeeds)
+
+# MONTECARLO (CURRENT SEASON PERF) => N
+dudes_simSeeds <- 1:WEEK |>
+  map_df(\(.WEEK, .SEASON, .points) {
+    .points |>
+      filter(season == .SEASON & week < .WEEK) |>
+      filter(week != 0) |>
+      nest(pts = pts, .by = c(id, playerId, pos)) |>
+      mutate(simType = "current_season_his",
+             seeds = map(pts, ~ .x$pts)) |>
+      select(-pts) |>
+      mutate(season = .SEASON, week = .WEEK)
+  }, .SEASON = SEASON, .points = points) |>
+  bind_rows(dudes_simSeeds)
+
+# SAMPLING FROM DENSITY(CURRENT SEASON PERF) => N
+dudes_simSeeds <- 1:WEEK |>
+  map_df(\(.WEEK, .SEASON, .points) {
+    .points |>
+      filter(season == .SEASON & week < .WEEK) |>
+      filter(week != 0) |>
+      nest(pts = pts, .by = c(id, playerId, pos)) |>
+      mutate(simType = "current_season_his_density",
+             seeds = map(pts, \(pts) {
+               if (nrow(pts) < 2)
+                 return(pts$pts)
+               den <- density(pts$pts)
+               i <-
+                 sample(length(den$x), 100, replace = T, prob = den$y)
+               return(den$x[i])
+             })) |>
+      select(-pts) |>
+      mutate(season = .SEASON, week = .WEEK)
+  }, .SEASON = SEASON, .points = points) |>
+  bind_rows(dudes_simSeeds)
+
+# MIX DENSITY PROJ_SOURCE_W_ERRORS + CURRENT_PERFORMANCE
+dudes_simSeeds <-
+  dudes_simSeeds |>
+  filter(
+    simType %in% c(
+      "current_season_his_density",
+      "proj_src_errors_density",
+      "proj_src_density"
+    )
+  ) |>
+  mutate(seeds = map(seeds, \(sds) {
+    if (length(sds) != 100) {
+      return(sample(sds, size = 100, replace = T))
+    } else {
+      return(sds)
+    }
+  })) |>
+  pivot_wider(
+    id_cols = c(season, week, id, playerId, pos),
+    names_from = simType,
+    values_from = seeds,
+    values_fn = c
+  ) |>
+  mutate(
+    seeds = map2(proj_src_density, proj_src_errors_density, c),
+    seeds = map2(seeds, current_season_his_density, c),
+    simType = "proj_src_w_error_current_season_density"
+  ) |>
+  select(season, week, id, playerId, pos, simType, seeds) |>
+  bind_rows(dudes_simSeeds) |>
   arrange(season, week, id, playerId, pos, simType)
-  
+
+
 library(dm)
-simDB <- dm(dudes_simSeeds) |> 
+simDB <- 
+  dudes_simSeeds |> 
+  dm(dudes_simSeeds) |> 
   dm_add_pk(dudes_simSeeds, c(season, week, id, playerId, pos, simType))
 
 saveRDS(simDB, "./data/dudes_simulation.rds")
