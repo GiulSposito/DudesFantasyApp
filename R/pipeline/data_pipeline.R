fd3bf0a91dd68f8f339dd5c92b130dd60d45cce5
Giuliano Sposito <gsposito@MMB026334CPS.local>
2023-11-20 10:02:41 -0300

    update week 11

--- R/pipeline/data_pipeline.R
+++ R/pipeline/data_pipeline.R
@@ -181,7 +181,7 @@ config <- yaml::read_yaml("./config/config.yml")
 .week <- 11L
 .leagueId <- config$leagueId
 .scoreRules <- yaml::read_yaml("./config/score_settings.yml")
-.tag <- "posTNF"
+.tag <- "preMNF"
 
 # update ffa_db ####
 source("./R/api/ffa_projection.R")
